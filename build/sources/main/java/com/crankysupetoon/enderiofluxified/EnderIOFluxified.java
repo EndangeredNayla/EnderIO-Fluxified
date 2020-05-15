41c7ad6e28cbcd40f2e6d30a9e584245cda18070
CrankySupertoon <crankysupertoon@gmail.com>
2020-05-15 13:31:25 -0400

    Clean Source

---
+++ build/sources/main/java/com/crankysupetoon/enderiofluxified/EnderIOFluxified.java
@@ -0,0 +1,9 @@
+package com.crankysupetoon.enderiofluxified;
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod(modid = EnderIOFluxified.MODID, version = EnderIOFluxified.VERSION)
+public class EnderIOFluxified {
+	public static final String MODID = "enderiofluxified";
+	public static final String VERSION = "1.0.0";
+	}
