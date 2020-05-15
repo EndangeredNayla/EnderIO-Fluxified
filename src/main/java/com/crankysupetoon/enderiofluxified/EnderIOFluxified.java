d9a0ed75232004d9a67d5ffd16839c394d9709e7
CrankySupertoon <crankysupertoon@gmail.com>
2020-05-15 13:37:20 -0400

    1.0.1, the one probe fix

--- src/main/java/com/crankysupetoon/enderiofluxified/EnderIOFluxified.java
+++ src/main/java/com/crankysupetoon/enderiofluxified/EnderIOFluxified.java
@@ -5,5 +5,5 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(modid = EnderIOFluxified.MODID, version = EnderIOFluxified.VERSION)
 public class EnderIOFluxified {
 	public static final String MODID = "enderiofluxified";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	}
