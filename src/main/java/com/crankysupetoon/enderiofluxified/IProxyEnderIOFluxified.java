4e1f1778d82c7154d9c9abd6110da156a81eac00
CrankySupertoon <crankysupertoon@gmail.com>
2020-05-15 12:49:43 -0400

    push

---
+++ src/main/java/com/crankysupetoon/enderiofluxified/IProxyEnderIOFluxified.java
@@ -0,0 +1,16 @@
+package com.crankysupetoon.enderiofluxified;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public interface IProxyEnderIOFluxified {
+	void preInit(FMLPreInitializationEvent event);
+
+	void init(FMLInitializationEvent event);
+
+	void postInit(FMLPostInitializationEvent event);
+
+	void serverLoad(FMLServerStartingEvent event);
+}
