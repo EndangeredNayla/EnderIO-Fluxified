324fd76679f6cef8b0b3a175e0f75931074a0f7d
Nora Hanegan <norahanegan@gmail.com>
2021-08-07 17:36:13 -0400

    1.0.3

--- src/main/java/com/unicornora/enderiofluxified/EnderIOFluxified.java
+++ src/main/java/com/unicornora/enderiofluxified/EnderIOFluxified.java
@@ -1,9 +1,9 @@
-package com.crankysupetoon.enderiofluxified;
+package com.unicornora.enderiofluxified;
 import net.minecraftforge.fml.common.Mod;
 
 
 @Mod(modid = EnderIOFluxified.MODID, version = EnderIOFluxified.VERSION)
 public class EnderIOFluxified {
 	public static final String MODID = "enderiofluxified";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	}
